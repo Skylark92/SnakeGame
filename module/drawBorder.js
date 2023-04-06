9ea897968860c8aab8ebbfc3251a2c58103d74f8
Skylark92 <skylark92@live.co.kr>
2023-04-07 00:07:30 +0900

    경계선 색, 점수 및 버튼 스타일 변경

--- module/drawBorder.js
+++ module/drawBorder.js
@@ -2,7 +2,7 @@ import { ctx, width, height, blockSize } from "../index.js";
 
 export default function drawBorder() {
   // 경계선 그리기
-  ctx.fillStyle = "Gray";
+  ctx.fillStyle = "#006bbd";
   // 경계선 색 지정
   ctx.fillRect(0, 0, width, blockSize);
   ctx.fillRect(0, height - blockSize, width, blockSize);
