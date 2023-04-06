30637fc8435d988964b1b3253fca71706075c86d
Skylark92 <skylark92@live.co.kr>
2023-04-07 00:01:59 +0900

    점수 표시 방법 변경

--- module/drawScore.js
+++ module/drawScore.js
@@ -1,10 +1,7 @@
-import { ctx, score, blockSize } from "../index.js";
+import { score } from "../index.js";
 
 export default function drawScore() {
   // 점수 표시하기
-  ctx.font = "20px Courier";
-  ctx.fillStyle = "Black";
-  ctx.textAlign = "left";
-  ctx.textBaseline = "top";
-  ctx.fillText("Score: " + score.score, blockSize, blockSize);
+  const p = document.querySelector(".snake-game-score");
+  p.innerText = score.score;
 }
